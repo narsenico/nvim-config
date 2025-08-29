2e4058fba0a98a66f02b14957c1e2a2899cc3da3
narsenico <Gianfranco.Caldi@zucchetti.it>
2025-08-29 20:42:00 +0200

    signcolumn limitato a 2 caratteri

--- init.lua
+++ init.lua
@@ -61,7 +61,7 @@ vim.pack.add({
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.signcolumn = "yes:3"
+vim.opt.signcolumn = "yes:2"
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.winborder = "rounded"
